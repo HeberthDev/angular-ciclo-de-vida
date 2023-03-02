fb70ce29d132fc76c0e0a8974951c1ef294c2374
HeberthDev <heberthdeveloper@gmail.com>
2023-03-01 22:32:20 -0300

    OnInit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'app-lista-de-compras';
+
+  public listaDeCompra! : Array<Item>
+
+  constructor(
+    private servicoListaDeCompra: ListaDeCompraService,
+  ) { }
+
+  ngOnInit(){
+    this.listaDeCompra = this.servicoListaDeCompra.getListaDeCompra();
+
+    console.log(this.listaDeCompra);
+  }
+}
