6cfa762a2c0bcbc6dbd5206e9dcdf2b8e1b5e774
HeberthDev <heberthdeveloper@gmail.com>
2025-02-20 07:25:12 -0300

    Adicionando novos itens à lista

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+  public valorItem !: string;
 
   constructor() { }
 
   ngOnInit(): void { }
+
+  public adicionarItem() {
+    console.log(this.valorItem);
+  }
 }
