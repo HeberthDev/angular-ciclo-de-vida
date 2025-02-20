52cb167d77eb85ce8ff0cdff64eefbc122b1282d
HeberthDev <heberthdeveloper@gmail.com>
2025-02-19 22:08:36 -0300

    Carregando dados com OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
+  public listaDeCompras !: Array<Item>;
 
-  constructor() { }
+  constructor(
+    private listaService: ListaDeCompraService,
+  ) { }
+
+  ngOnInit(): void {
+    this.listaDeCompras = this.listaService.getListaDeCompra();
+    console.log('listaDeCompras -> ', this.listaDeCompras);
+  }
 }
